1f82f22da58a2bf9e4aa09241d25387155c146ff
atikailma <atikailma28@gmail.com>
2019-09-09 18:34:53 +0700

    duapuluh

--- send.php
+++ send.php
@@ -1,32 +1,45 @@
 <?php
-$name=$_POST['name'];
-$email=$_POST['email'];
-$subject=$_POST['subject'];
-$message=$_POST['message'];
+// Import PHPMailer classes into the global namespace
+// These must be at the top of your script, not inside a function
+use PHPMailer\PHPMailer\PHPMailer;
+use PHPMailer\PHPMailer\Exception;
+require 'phpmailer/Exception.php';
+require 'phpmailer/PHPMailer.php';
+require 'phpmailer/SMTP.php';
+//Load composer's autoloader
+//require 'vendor/autoload.php';
 
-$to="";
+$mail = new PHPMailer(true);                              // Passing `true` enables exceptions
+try {
+    //Server settings
+    $mail->SMTPDebug = 2;                                 // Enable verbose debug output
+    //$mail->isSMTP();                                      // Set mailer to use SMTP
+    $mail->Host = 'smtp.gmail.com';  // Specify main and backup SMTP servers
+    $mail->SMTPAuth = true;                               // Enable SMTP authentication
+    $mail->Username = 'test@gmail.com';                 // SMTP username
+    $mail->Password = '';                  // SMTP password
+    $mail->SMTPSecure = 'tls';                            // Enable TLS encryption, `ssl` also accepted
+    $mail->Port = 587;                                    // TCP port to connect to
+    $name = $_POST['name'];
+    $email = $_POST['email'];
+    //Recipients
+    $mail->setFrom($email, $name);
+    $mail->addAddress('test@gmail.com','satusoft');     // Add a recipient
+   // $mail->addAddress('address@example.com');               // Name is optional
+   //$mail->addReplyTo('address@example.com', 'example');
+   // $mail->addCC('cc@example.com');
+  //  $mail->addBCC('bcc@example.com');
+  $subject = $_POST['subject'];
+  $subject2 = $subject." from ".$name. " from email " .$email;
+    //Content
+    $mail->isHTML(true);                                  // Set email format to HTML
+    $mail->Subject = $subject2;
+    $mail->Body = $_POST['message'];
+    $mail->AltBody = 'This is the body in plain text for non-HTML mail clients';
 
-$message="Dear, <br /> <br />".$message;
-
-$headers = "MIME-Version: 1.0" . "\r\n";
-$headers .= "Content-type:text/html;charset=iso-8859-1" . "\r\n";
-
-// More headers
-$headers .= 'From:'.$name.' <'.$email.'>'."\r\n" . 'Reply-To: '.$name.' <'.$email.'>'."\r\n";
-$headers .= 'Cc: admin@gmail.com' . "\r\n"; //untuk cc lebih dari satu tinggal kasih koma
-@mail($to,$subject,$message,$headers);
-if (@mail) { ?>
-<script language="javascript" type="text/javascript">
-alert('Thank you for the message. We will contact you shortly.');
-window.location = 'index.html';
-</script>
-<?php
+    $mail->send();
+    echo 'Message has been sent';
+} catch (Exception $e) {
+    echo 'Message could not be sent.';
+    echo 'Mailer Error: ' . $mail->ErrorInfo;
 }
-else { ?>
-<script language="javascript" type="text/javascript">
-alert('Message failed. Please, send an email to xxxx@email.co.id');
-window.location = 'index.html';
-</script>
-<?php
-}
-?>
